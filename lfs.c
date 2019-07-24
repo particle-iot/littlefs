fbce90128cb334223f1ee19bf6413279e15aa7af
Andrey Tolstoy <avtolstoy@gmail.com>
2019-07-24 20:57:32 +0700

    include lfs_util.h before lfs.h

--- lfs.c
+++ lfs.c
@@ -15,8 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "lfs.h"
 #include "lfs_util.h"
+#include "lfs.h"
 
 
 /// Caching block device operations ///
